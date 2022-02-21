d80eaf18f9a2c6ee61c040bca5139263343d5536
LothaireE <lothaire.epee@gmail.com>
2022-02-21 15:31:35 +0100

    initial my-first-app

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Image,
+} from "react-native";
+import Constant from "expo-constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    // <StatusBar style="auto" />
+    // <StatusBar style="auto" />
+    //  <StatusBar style="auto" />
+    // </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+
+      <View>
+        <View style={styles.topbar}>
+          <Text>logo à venir</Text>
+          <Image
+            source={require("./assets/logo-IMDb-4.jpeg")}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "red",
+    // marginTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+  },
+  topbar: {
+    borderWidth: 1,
+    backgroundColor: "gray",
+    height: 100,
+  },
+  logo: {
+    height: 100,
+    width: 200,
   },
 });
